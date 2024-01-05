66e0121a6b2a94821f7f11469c9c7aa2e0a38069
Tanapron Tangpadungsuk <tt.icy013@gmail.com>
2024-01-05 11:12:49 +0700

    Lab13.2 Complete

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,26 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image[][M]){
+    cout << "------------------------------------------------------------------------\n";
+    for(int i = 0; i< N ;i++ ){
+        cout << "|";  
+        for(int j = 0 ; j < M; j++){
+            if( image[i][j] ==1 ) cout << "*";
+            else cout << " ";
+        }
+        cout << "|";
+        cout << endl;
+        }
+    cout << "------------------------------------------------------------------------\n";
+    }
+    
+void updateImage(bool image[][M],int s,int x,int y){
+   for(int i=0;i<N;i++){
+       for(int j=0;j<M;j++){
+           if (sqrt(pow(i-x,2)+pow(j-y,2)) <=s-1)
+               image[i][j] = 1;
+            }
+        }
+}
